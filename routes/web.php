01f8beaf1eaeef175ec0ed9985eadf71ba91c0dc
Pawan Nagar <pawannagar2243@gmail.com>
2026-02-15 16:30:28 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,16 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\AdminController;
+use Illuminate\Support\Facades\Mail;
+
+Route::get('/test-mail', function () {
+    Mail::raw('Ye test mail hai Laravel + SendGrid ke through!', function ($message) {
+        $message->to('yourgmail@gmail.com') // yahan aapka Gmail
+                ->subject('Test Mail from Laravel + SendGrid');
+    });
+
+    return 'Mail sent!';
+});
 
 Route::get('/', function () {
     return view('home');
